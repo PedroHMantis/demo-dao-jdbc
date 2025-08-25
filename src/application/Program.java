4bbedf2ce8e366778cdcb3dc7a6390b6e270e1ea
PedroHMantis <gtx1050pd@gmail.com>
2025-08-25 19:57:31 -0300

    Insert Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,14 @@ public class Program {
 
         for(Seller obj : list){
             System.out.println(obj);
+
         }
+
+        System.out.println("\n===== TEST 4: seller Insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00,department);
+
+        sellerDao.insert(newSeller);
+
+        System.out.println("Inseted! new id = " + newSeller.getId());
     }
 }
